43c6174664609f0806c820f27b8f9cd4ae1f4187
javiprada1 <javiprada1@gmail.com>
2022-10-03 19:36:36 +0200

    add dinamic form

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,29 @@
-console.log("EEEEEEE");
+
+
+$("#btn2").addEventListener("click", function () {
+    var input = document.createElement("input");
+    console.log(input);
+});
+function $(selector){
+    return document.querySelector(selector);
+}
+
+
+
+function myAlert(msg){
+    console.log($("body").children[1]);
+    var div = document.createElement("div");
+    div.classList.add("alert");
+    div.innerHTML=msg;
+    var close = document.createElement("span");
+    close.style.float="right";
+    close.classList.add("close");
+    close.innerHTML="x";
+    div.appendChild(close);
+    $("body").insertBefore(div,$("body").firstChild);
+    bind_close();
+}
+
+
+
+
