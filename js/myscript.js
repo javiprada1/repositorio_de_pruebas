856046e8f97283eab7fc163b94c2366972e80bab
javiprada1 <javiprada1@gmail.com>
2022-10-03 19:38:13 +0200

    improve dinamic form

--- js/myscript.js
+++ js/myscript.js
@@ -1,27 +1,37 @@
 
-
-$("#btn2").addEventListener("click", function () {
-    var input = document.createElement("input");
-    console.log(input);
+//creando muchos input de email
+$("#btn2").addEventListener("click",function(){
+    var input=document.createElement("input");
+    input.setAttribute("type","email");
+    input.setAttribute("placeholder","E-mail");
+    input.setAttribute("name","E-mail[]");
+    $("#form").appendChild(input);
+    myAlert("add new email");
 });
 function $(selector){
     return document.querySelector(selector);
 }
-
-
-
 function myAlert(msg){
-    console.log($("body").children[1]);
-    var div = document.createElement("div");
+    //console.log($("body").children[1]);
+    var div=document.createElement("div");
     div.classList.add("alert");
     div.innerHTML=msg;
-    var close = document.createElement("span");
+    var close=document.createElement("span");
     close.style.float="right";
     close.classList.add("close");
-    close.innerHTML="x";
+    close.innerHTML="X";
     div.appendChild(close);
     $("body").insertBefore(div,$("body").firstChild);
     bind_close();
+}
+function bind_close(){
+    let elements =document.querySelectorAll(".close");
+    for(var i=elements.length-1;i>=0;i--){
+        let el=elements[i];
+        el.addEventListener("click",function(){
+            this.parentNode.style.display="none";
+        });
+    }
 }
 
 
