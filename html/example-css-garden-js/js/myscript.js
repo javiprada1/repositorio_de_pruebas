5b79301ba7a13ca4dd1ff16506a1014a642d692a
Javier Prada <javiprada1@gmail.com>
2022-11-19 10:33:44 +0100

    add funcionality change css on click with jquery

---
+++ html/example-css-garden-js/js/myscript.js
@@ -0,0 +1,26 @@
+
+
+/*
+*
+* Cambiamos estilos según preferencia de usuario
+*
+*/
+    $("#style1").click(function() {
+        $('link[media="screen"]').attr('href','css/style1.css');
+      });
+
+      $("#style2").click(function() {
+        $('link[media="screen"]').attr('href','css/style2.css');
+      });
+
+      $("#style3").click(function() {
+        $('link[media="screen"]').attr('href','css/style3.css');
+      });
+
+      $("#style4").click(function() {
+        $('link[media="screen"]').attr('href','css/style4.css');
+      });
+
+      $("#style5").click(function() {
+        $('link[media="screen"]').attr('href','css/style5.css');
+      });
